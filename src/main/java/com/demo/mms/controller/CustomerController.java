6ae87c84521562ec37e7bbce26d3bbba45ff340f
Billythecrescent <13550378056@163.com>
2019-07-18 23:07:57 +0800

    alter CustomerController.jsp

--- src/main/java/com/demo/mms/controller/CustomerController.java
+++ src/main/java/com/demo/mms/controller/CustomerController.java
@@ -4,7 +4,6 @@ import com.demo.mms.common.domain.Customer;
 import com.demo.mms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
